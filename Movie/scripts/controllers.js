796e70e17e2d273335c8d00dc5c2b5fb7f4281f8
ntio7 <ntio7@walla.com>
2020-02-01 19:26:29 +0200

    init

--- Movie/scripts/controllers.js
+++ Movie/scripts/controllers.js
@@ -5,43 +5,22 @@
 var MovieAppControllers = angular.module("MovieAppControllers", []);
 
 MovieAppControllers.controller('MovieController', function ($scope, getMovies) {
+    $scope.ShowList = true;
     getMovies.getAllMovies().then(function (res) {
-        $scope.movies = [];
+        $scope.movies = (JSON.parse(res.data)).Search;
         let mdata = JSON.parse(res.data);
-        $.each((mdata.Search), function (i, field) {
-            let movieID = '';
-            movieID = field.imdbID;
-            getMovies.getEachMovies(movieID).then(function (res) {
-                let movie = JSON.parse(res.data);
-                $scope.movies.push(movie);
-                console.log($scope.movies);
 
-            });
-        });
     });
   
-    $scope.searchYear = function (n, a, d) {        ;
-        getMovies.getAllMovies().then(function (res) {
-            $scope.movies = [];
-            $.each((res.data.Search), function (i, field) {
-                let movieID = '';
-                movieID = field.imdbID;
-                getMovies.getEachMovies(movieID).then(function (res) {
-                    let movie = res.data;
-                    let movies = [];
-                    movies.push(movie);
-                    $.each(movies, function (i, field2) {
-                        let year = field2.Year;
-                        if (year.startsWith(n)) {
-                            $scope.movies.push(field2);
-                        }
-                    });
-                });
+    $scope.ShowDetails = function (movie) {
+
+        $scope.ShowList = !$scope.ShowList;
+        if (!$scope.ShowList) {
+            getMovies.getEachMovies(movie.imdbID).then(function (res) {
+                let movie = JSON.parse(res.data);
+                $scope.movie = movie;
             });
-        });
+        }
     };
-
-    
-
 });
 
--- Movie/scripts/services.js
+++ Movie/scripts/services.js
@@ -4,16 +4,16 @@
 
 var MovieAppServices = angular.module('MovieAppServices', ['ngResource']);
 
-MovieAppServices.factory('getMovies', function($http){
-  var factory = {}
+MovieAppServices.factory('getMovies', function ($http) {
+    var factory = {};
 
-  factory.getAllMovies = function(){
-      return $http.get("https://localhost:44391/api/movie/");
-  }
+    factory.getAllMovies = function () {
+        return $http.get("https://localhost:44391/api/movie/");
+    };
 
-  factory.getEachMovies = function(id){
-      return $http.get("https://localhost:44391/api/movie/" + id);
-  }
-  
-  return factory
-})
+    factory.getEachMovies = function (id) {
+        return $http.get("https://localhost:44391/api/movie/" + id);
+    };
+
+    return factory
+});
